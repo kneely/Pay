a41a0b618192a72bbe5a061901ffcd678656bdfd
ERAU MSSA Student <ERAU MSSA Student>
2018-03-19 10:32:30 -0400

    Add project files.

---
+++ Pay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
